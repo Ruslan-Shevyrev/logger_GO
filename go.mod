c49f90abc55807aa909fb7c979edde614fbd7f84
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2026-06-02 10:10:25 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ruslan-Shevyrev/logger_GO
+
+go 1.21
---
+++ logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"log"
+)
+
+type Logger struct {
+	URL      string
+	Username string
+	Password string
+}
+
+func New(url, username, password string) *Logger {
+	return &Logger{
+		URL:      url,
+		Username: username,
+		Password: password,
+	}
+}
+
+func (l *Logger) Log(message string, level string) {
+	// Здесь логика отправки в grafana
+	log.Printf("[%s] [grafana %s]: %s \n",
+		level, l.URL, message)
+}
